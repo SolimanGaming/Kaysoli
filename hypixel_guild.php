39de0aac501268e6a7fc983ec80236fbca17549b
Dawood Yusef <91003107+SolimanGaming@users.noreply.github.com>
2025-08-22 22:18:38 +0300

    Update hypixel_guild.php

--- hypixel_guild.php
+++ hypixel_guild.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
 $apiKey = "4f0bdb4c-572c-4571-b5b3-4906063bcfd8"; // Replace with your Hypixel API key
-$uuid = $_GET['uuid'] ?? '';
+$uuid = "ae24c7c71f4442c2aa406d218e6c1d8c";
 
 if (!$uuid) {
     echo json_encode(["error" => "No UUID provided"]);
